fd9b82239b084269f85472d6f42cf33a3813e74d
NickLojas <nick.lojas@mail.utoronto.ca>
2024-05-27 11:42:20 -0400

    Adding autotag, bufferline and colorizer

--- init.lua
+++ init.lua
@@ -942,7 +942,10 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.bufferline',
+  require 'kickstart.plugins.colorizer',
+  require 'kickstart.plugins.autotag',
+  require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.git', -- adds gitsigns recommend keymaps
   require 'kickstart.plugins.harpoon',
